5da428316f4de97c9a05b00f3258adbccbf8dd6c
Пользователь <kari.inago@icloud.com>
2019-10-31 17:16:03 +0300

    ДЗ1

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace ДЗ1 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Доброго дня,{name}");
+            Thread.Sleep(5000);
+            Console.WriteLine($"До скорых встречь,{name}");
+            Console.ReadKey();
+        }
+    }
+}
